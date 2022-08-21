af6387974eb405e6c1a373f6d993fc40d18914eb
EduardMiclos <micloseduard@yahoo.com>
2022-08-21 16:13:13 +0300

    Created Song class

---
+++ MusicManager/Song.py
@@ -0,0 +1,6 @@
+class Song:
+    def __init__(self, title: str = "", artist: str = "Unknown", genre: str = "", path: str = "") -> None:
+        self.title = title
+        self.artist = artist
+        self.genre = genre
+        self.path = path

f87993289ab7a51fa85336839c77899366be1db1
EduardMiclos <micloseduard@yahoo.com>
2022-08-29 21:22:23 +0300

    Updated BluetoothManager + VoiceRecognition
    Created is_connected() and get_device_info() methods
    inside BluetoothManager and fixed some issues.
    Now everything seems to work fine.
    
    Also, created and programmed the VoiceRecognition module, but I'm
    unable to test it because I need a microphone for RaspberryPi.

--- BluetoothManager/BluetoothManager.py
+++ BluetoothManager/BluetoothManager.py
@@ -1,18 +1,22 @@
 import subprocess
 from subprocess import CalledProcessError
+from typing import List
 
 class BluetoothManager:
     def __init__(self, target_uuid) -> None:
         self.target_uuid = target_uuid
         self.device_name = None
 
-    def connect(self, target_uuid = self.target_uuid) -> bool:
-        try:
-            subprocess.run(['bluetoothctl', 'connect', target_uuid], check = True)
-            process = subprocess.run(['bluetoothctl', 'info', self.target_uuid], check = True, capture_output = True)
+    def connect(self, target_uuid = None) -> bool:
+        if target_uuid is not None:
+            self.target_uuid = target_uuid
 
-            self.device_name = str(process.stdout).split('\\n\\t')[1]
-            self.device_name = device_name.split(':')[1].strip()
+        try:
+            subprocess.run(['bluetoothctl', 'connect', self.target_uuid], check = True)
+
+            device_info = self.get_device_info()
+            self.device_name = device_info[1]
+            self.device_name = self.device_name.split(':')[1].strip()
 
             print(f'Successfully connected to device: {self.device_name}')
             return True
@@ -23,6 +27,9 @@ class BluetoothManager:
             return False
 
     def disconnect(self) -> bool:
+        if not self.is_connected():
+            return False
+
         try:
             subprocess.run(['bluetoothctl', 'disconnect', self.target_uuid], check = True)
 
@@ -38,9 +45,25 @@ class BluetoothManager:
 
             return False
 
+    def get_device_info(self) -> List[str]:
+        process = subprocess.run(['bluetoothctl', 'info', self.target_uuid], capture_output = True)
+        str_process = str(process.stdout)
 
+        if "not available" in str_process:
+            print('Failure: The device is not available!')
+            return None
+
+        return str_process.split('\\n\\t')
+
+    def is_connected(self) -> bool:
+        device_info = self.get_device_info()
+
+        is_connected = ""
+
+        if device_info is not None:
+            is_connected = device_info[8]
+            
+        return True if 'yes' in is_connected else False
 
 bm = BluetoothManager(target_uuid = '2C:FD:B4:38:5D:CA')
-bm.connect()
-
 

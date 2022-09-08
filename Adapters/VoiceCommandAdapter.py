6638561a487dc71a9536cf022627296ec0f3efd7
EduardMiclos <micloseduard@yahoo.com>
2022-09-08 20:39:31 +0300

    Changed threads to multiprocessing
    Need to find a method to kill all children processes (including the ones created by speake module).

--- Adapters/VoiceCommandAdapter.py
+++ Adapters/VoiceCommandAdapter.py
@@ -2,6 +2,8 @@ import sys, os
 import Levenshtein
 import speake3
 
+MAX_LEVENSHTEIN_DISTANCE_ERROR = 3
+
 class VoiceCommandAdapter:
     def __init__(self, voice_cmds) -> None:
         self.voice_cmds = voice_cmds    
@@ -15,10 +17,20 @@ class VoiceCommandAdapter:
         self.engine.set('pitch', '40')
 
     # Return a function based on the voice command.
-    def interpret(self, command: str) -> object: 
+    def interpret(self, command: str, fallback: object) -> object: 
         cmds_dict = {cmd_text:[Levenshtein.distance(command, cmd_voice[0]), cmd_voice[1]] for cmd_text, cmd_voice in self.voice_cmds}
-        key_with_min_distance = min(cmds_dict, key = cmds_dict.get)
-        return cmds_dict[key_with_min_distance][1]
+
+        min_key = None
+        min_val = MAX_LEVENSHTEIN_DISTANCE_ERROR
+        for key, val in cmds_dict.items():
+            if val[0] < min_val:
+                min_val = val[0]
+                min_key = key
+
+        if min_key is None:
+            return fallback
+        return cmds_dict[min_key][1]
+
 
     # This function represent the speaking mechanism of the Raspberry Pi.
     def speak(self, text: str) -> bool:
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import multiprocessing
+from ctypes import c_wchar_p
+
 from Adapters.VoiceCommandAdapter import VoiceCommandAdapter
 from MusicManager.MusicManager import MusicManager
-
+from VoiceRecognition.VoiceListener import VoiceListener
 
 class MusicSystem:
     def __init__(self) -> None:
@@ -26,8 +29,38 @@ class MusicSystem:
         self.music_manager = MusicManager()
         self.voice_adapter = VoiceCommandAdapter(voice_cmds = self.voice_cmds.items())
 
+        self.SYSTEM_ON = False
+        self.input_command = None
+
+    def turn_on(self) -> None:
+        self.SYSTEM_ON = True 
+        self.input_command = multiprocessing.Manager().Value(c_wchar_p, '')
+
+        # This signal is used for communication between the current thread and voice_listener thread.
+        interrupt_signal = multiprocessing.Event()
+
+        # This process takes input commands from the user.
+        voice_listener = multiprocessing.Process(target = VoiceListener.listen, args = (interrupt_signal, self.input_command), daemon = False) 
+        voice_listener.start()
+
+        # This process executes all the commands. It can be interrupted at any time.
+        command_executer = multiprocessing.Process(daemon = False)
+
+        while self.SYSTEM_ON:
+            interrupt_signal.wait()
+
+            if command_executer.is_alive():
+                command_executer.kill()
+
+            callback = self.voice_adapter.interpret(command = self.input_command.value, fallback = self.unknown_command)
+            
+            command_executer = multiprocessing.Process(target = callback, daemon = True)
+            command_executer.start()
+
+            interrupt_signal.clear()
+        
     def play_genre(self) -> None:
-        self.voice_adapter.speak('Please pick a genre')
+        self.voice_adapter.speak('Please pick a genreaa asdf sdf wefsdf efsdf srdsd sd sd sdrsd fsdfsdfsdf sdr sd r')
 
     def play_song(self) -> None:
         self.voice_adapter.speak('Please pick a song')
@@ -47,9 +80,14 @@ class MusicSystem:
     def stop(self) -> None:
         self.voice_adapter.speak('Stopping')
 
+    def unknown_command(self) -> None:
+        self.voice_adapter.speak('I\'m sorry. I don\'t understand what you\'re saying')
+
     def turn_off(self) -> None:
         self.voice_adapter.speak('Turning off. Goodbye!')
 
+        self.SYSTEM_ON = False
+
 
 ms = MusicSystem()
-ms.voice_adapter.interpret("downgrade new ong")()
+ms.turn_on()

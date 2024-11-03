40c50e00f3463e8110ace90eb3cc795e2b4a297e
Eduard Miclos <micloseduard@yahoo.com>
2024-11-03 23:46:28 +0200

    Updated database schema & created new database

---
+++ Database/create_schema.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+conn = sqlite3.connect('songs.db')
+cursor = conn.cursor()
+
+cursor.execute("DROP TABLE IF EXISTS artist;")
+cursor.execute("DROP TABLE IF EXISTS song;")
+cursor.execute("DROP TABLE IF EXISTS genre;")
+
+cursor.execute('''
+    CREATE TABLE genre (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL
+    );
+''')
+
+cursor.execute('''
+    CREATE TABLE song (
+        id INTEGER PRIMARY KEY,
+        yt_playlist_id TEXT,
+        yt_song_id TEXT,
+        genre_id INTEGER,
+        title TEXT NOT NULL,
+        last_listened DATETIME,
+        last_listened_percent INTEGER,
+        duration_seconds INTEGER,
+        play_count INTEGER,
+        FOREIGN KEY (genre_id) REFERENCES genre(id)
+    );
+''')
+
+cursor.execute('''
+    CREATE TABLE artist (
+        id INTEGER PRIMARY KEY,
+        song_id INTEGER,
+        name TEXT NOT NULL,
+        FOREIGN KEY (song_id) REFERENCES song(id)
+    );
+''')
+
+conn.commit()
+conn.close()
+
+print("Database 'songs.db' and tables created successfully.")
